eae0f23bfdb86fbe2f325e48c09bedef77836496
Abinash <abinash0989@gmail.com>
2026-02-08 15:09:57 +0530

    Removed Client Stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ParallaxShowcase from '@/components/home/ParallaxShowcase';
 import AboutSnippet from '@/components/home/AboutSnippet';
 import FeaturedProjects from '@/components/home/FeaturedProjects';
 import ServicesOverview from '@/components/home/ServicesOverview';
-import Testimonials from '@/components/home/Testimonials';
 import CTASection from '@/components/home/CTASection';
 
 export default function HomePage() {
@@ -14,7 +13,6 @@ export default function HomePage() {
       <AboutSnippet />
       <FeaturedProjects />
       <ServicesOverview />
-      <Testimonials />
       <CTASection />
     </>
   );
